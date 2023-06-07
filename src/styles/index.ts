4d75e2f531c97cd488b8cc53a9ea420ee3d599f2
Leandro Rangel <rangel94@outlook.com>
2023-06-07 18:00:30 -0300

    Estrutura visual - Configurando Stitches

---
+++ src/styles/index.ts
@@ -0,0 +1,18 @@
+import { createStitches } from "@stitches/react";
+
+export const {
+  config,
+  styled,
+  css,
+  globalCss,
+  theme,
+  createTheme,
+  keyframes,
+  getCssText,
+} = createStitches({
+  theme: {
+    colors: {
+      rocketseat: "#8257e6",
+    },
+  },
+});
