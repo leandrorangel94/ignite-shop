1e816e47e4cd29b49fd451f4e03ae7af8057d973
Leandro Rangel <rangel94@outlook.com>
2023-06-11 12:42:33 -0300

    Produto & Checkout - Estrutura da página de produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,30 @@
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from "@/src/styles/pages/product";
 import { useRouter } from "next/router";
 export default function Product() {
   const { query } = useRouter();
   return (
     <>
-      <h1>Product Page {JSON.stringify(query)}</h1>
+      <ProductContainer>
+        <ImageContainer></ImageContainer>
+
+        <ProductDetails>
+          <h1>Camiseta X</h1>
+          <span>R$ 79,90</span>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi fuga
+            inventore maiores voluptatum similique? Laborum ratione harum
+            aspernatur recusandae hic? Error porro maiores esse inventore odio
+            sapiente expedita, corrupti quibusdam!
+          </p>
+
+          <button>Comprar agora</button>
+        </ProductDetails>
+      </ProductContainer>
     </>
   );
 }
