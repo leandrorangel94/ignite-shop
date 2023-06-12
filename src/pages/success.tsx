0b45cf196992797830634d3d4b660d1d77c86b2c
Leandro Rangel <rangel94@outlook.com>
2023-06-12 12:12:19 -0300

    Produto & Checkout - Estrutura de sucesso

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,3 +1,19 @@
+import Link from "next/link";
+import { ImageContainer, SuccessContainer } from "../styles/pages/success";
+
 export default function Success() {
-  return <h1>Success Page</h1>;
+  return (
+    <SuccessContainer>
+      <h1>Compra efetuada!</h1>
+
+      <ImageContainer></ImageContainer>
+
+      <p>
+        Uhuul <strong>Leandro Rangel</strong>, Sua <strong>Camiseta X</strong>{" "}
+        já está a caminho da sua casa!
+      </p>
+
+      <Link href="/">Voltar ao catálogo</Link>
+    </SuccessContainer>
+  );
 }
