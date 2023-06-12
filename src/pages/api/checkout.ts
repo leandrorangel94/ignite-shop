f83a2594159e9e25c20ae022639fdc109909a737
Leandro Rangel <rangel94@outlook.com>
2023-06-12 10:08:29 -0300

    Produto & Checkout - Botão de comprar

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -5,7 +5,19 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const priceId = "price_1NH6lMCWRrbZMzp672WizLd8";
+  const { priceId } = req.body;
+
+  if (req.method !== "POST") {
+    res.status(405).json({
+      error: "Method not allowed.",
+    });
+  }
+
+  if (!priceId) {
+    res.status(400).json({
+      error: "Price not found.",
+    });
+  }
 
   const successUrl = `${process.env.NEXT_URL}/success`;
   const cancelUrl = `${process.env.NEXT_URL}/`;
